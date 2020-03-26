0930c2ee09f5776f77afd4e52bb7bc7fd51b99af
Reid Mewborne <rmewborne@me.com>
2020-03-26 16:17:34 -0400

    using scalafmt now

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,4 +1,5 @@
 package example
+
 import caliban.{CalibanError, GraphQL, ResponseValue}
 import caliban.wrappers.Wrappers.{maxDepth, printSlowQueries, timeout}
 import caliban.wrappers.ApolloTracing.apolloTracing
@@ -15,7 +16,7 @@ object Hello extends zio.App {
 
   case class Character(name: String, age: Int)
 
-  private val CharactersDb = List(Character("reid", 32))
+  private val CharactersDb = List(Character("reid", 32), Character("lauren", 28))
 
   def getCharacters: List[Character] = CharactersDb
   def getCharacter(name: String): Option[Character] = CharactersDb.find(c => c.name == name)
@@ -23,26 +24,25 @@ object Hello extends zio.App {
   // Our API
   // schema
   case class CharacterName(name: String)
-  case class Queries(characters: List[Character],
-                     character: CharacterName => Option[Character])
+  case class Queries(characters: List[Character], character: CharacterName => Option[Character])
   // resolver
   val queries = Queries(getCharacters, args => getCharacter(args.name))
 
   import caliban.GraphQL.graphQL
   import caliban.RootResolver
 
-  val api: GraphQL[Console with Clock] = graphQL(RootResolver(queries))@@
-      maxDepth(50) @@
-      timeout(3.second) @@
-      printSlowQueries(500.millis) @@
-      apolloTracing
+  val api: GraphQL[Console with Clock] = graphQL(RootResolver(queries)) @@
+    maxDepth(50) @@
+    timeout(3.second) @@
+    printSlowQueries(500.millis) @@
+    apolloTracing
 
   case class GraphQLResponse[+E](data: ResponseValue, errors: List[E])
 
   val query: String =
     """
       |{
-      |   characters {
+      |   character(reid) {
       |     name
       |   }
       |}
@@ -50,12 +50,6 @@ object Hello extends zio.App {
 
   lazy val myAppLogic: ZIO[Console with Clock, CalibanError.ValidationError, Unit] = for {
     _           <- putStrLn(api.render)
-    /*
-      In order to process requests, you need to turn your API into an interpreter,
-      which can be done easily by calling .interpreter. An interpreter is a light
-      wrapper around the API definition that allows plugging in some middleware and
-      possibly modifying the environment and error types (see Middleware for more info).
-     */
     interpreter <- api.interpreter
     result      <- interpreter.execute(query)
     _           <- putStrLn(result.data.toString)
